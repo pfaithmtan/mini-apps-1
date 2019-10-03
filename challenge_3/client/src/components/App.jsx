3f132f49888abab8886d491f2c7145053258d22b
Faith <pfaithmtan@gmail.com>
2019-10-02 20:02:02 -0700

    Init

---
+++ challenge_3/client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Sup
+            </div>
+        );
+    }
+}
+
+export default App;
